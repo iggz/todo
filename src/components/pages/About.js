47a67de2877a86a496bbb289ca94c674bfbf00cc
iggz <ipopenov@protonmail.com>
2019-12-06 20:05:41 -0500

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <React.Fragment>
+                <h1>About</h1>
+                <p>This is the TodoList app v1.0.0.</p>
+            </React.Fragment>
+        </div>
+    )
+}
+
+export default About;
